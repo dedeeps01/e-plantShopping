79547aadf1a4ab43ec019e84a6c9188b89e7dae0
dedeeps01 <69516049+dedeeps01@users.noreply.github.com>
2025-02-21 00:20:22 -0500

    Update CartSlice.jsx

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -16,7 +16,9 @@ export const CartSlice = createSlice({
         }
     },
     removeItem: (state, action) => {
-        state.items = state.items.filter(item => item.name !== action.payload.name);
+        const item = action.payload;
+        state.items = state.items.filter((i) => i.name !== item.name);
+//        state.items = state.items.filter(item => item.name !== action.payload.name);
     },
     updateQuantity: (state, action) => {
         const { name, quantity } = action.payload;
